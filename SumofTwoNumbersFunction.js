46ff46c914a1300bc4b8a13f18436b030ab0f917
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-04-21 15:28:51 +0000

    Create SumofTwoNumbersFunction.js
    A function for adding two input numbers and displaying the result in Javascript.

---
+++ SumofTwoNumbersFunction.js
@@ -0,0 +1,12 @@
+function SumofTwoNumbers(a,b); //intializing function
+{
+  //addition of the two numbers;
+console.log("The Sum is" + result);//Displays the sum
+  
+}
+
+let a= parsFloat(Prompt("Enter The First Number: "))
+let b= parseFloat(Prompt("Enter The Second Number:"))
+
+let result=a+b;  
+return result;
