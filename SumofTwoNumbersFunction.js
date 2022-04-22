b33e5e0a8ad172023e1f0fac1eae75934c5777ea
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-04-22 13:28:42 +0300

    Update SumofTwoNumbersFunction.js

--- SumofTwoNumbersFunction.js
+++ SumofTwoNumbersFunction.js
@@ -21,3 +21,12 @@ for (var i=1; i<=10;i++)
   sum=+i;
 }
 console.log(sum);
+
+//SUM OF ODD NUMBERS BETWEEN A SPECIFIED RANGE (>10 && <30)
+let sum=0;
+for (var num=11; num<=30; num++)
+{
+    sum = sum+num;
+    }
+    
+    console.log(sum);
