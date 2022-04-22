91ba47822e831e2e8ce31d6a894643b3a658d9c3
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-04-22 12:42:50 +0300

    Create NumberSequence.js
    While loop used to print numbers in a simple sequence in Javascript

---
+++ NumberSequence.js
@@ -0,0 +1,18 @@
+//PRINTING POSITIVE NUMBERS FROM 1 TO 10
+
+let n=1; //insitializing the lowest number in the count
+while (n <= 10) //setting the condition
+{
+    console.log(n); //Printing the number to the console
+    n+=1 ; //number increment
+  }
+
+//PRINTING ODD NUMBERS FROM 1 TO 100
+let n=1;
+while (n <= 100) //setting the condition
+{
+    console.log(n);
+    n =(2* n) +1 ; //Increment Formula for finding Odd numbers
+  }
+
+
