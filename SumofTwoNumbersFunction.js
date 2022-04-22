f57b9fc2b32f9876f0da4a12639382291fe56bba
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-04-22 13:23:35 +0300

    Update SumofTwoNumbersFunction.js

--- SumofTwoNumbersFunction.js
+++ SumofTwoNumbersFunction.js
@@ -1,4 +1,7 @@
+//FUNCTION FOR SUM OF TWO NUMBERS
+
 function SumofTwoNumbers(a,b); //intializing function
+
 {
  let result=a+b;    //addition of the two numbers;
 console.log("The Sum is" + result);//Displays the sum
@@ -11,3 +14,10 @@ let b= parseFloat(Prompt("Enter The Second Number:")) //taking the second input 
 let Sum= SumofTwoNumbers();
 return;
 
+//SUM OF NUMBERS BETWEEN 1-10
+let sum=0;
+for (var i=1; i<=10;i++)
+{
+  sum=+i;
+}
+console.log(sum);
