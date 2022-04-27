0f71f26b5e9f46fd1ec39d25954435ca5d43f811
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-04-27 17:44:17 +0000

    Add files via upload
    if else loop

---
+++ Simple If else loop.js
@@ -0,0 +1,17 @@
+let n = 90;  
+if (n >= 80) {
+   console.log("A");
+  }
+else if (n>=70)
+{ 
+    console.log("B");
+}
+
+else if ( n >=60 ) {
+  console.log("C");
+}
+else {
+  console.log("Pass");
+}
+
+
