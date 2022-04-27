cdec29f8e5f5383eb4bf24a0c851129611d8c891
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-04-27 17:44:37 +0000

    Create If else loop.js
    If else loop

---
+++ If else loop.js
@@ -0,0 +1,16 @@
+let n = 90;  
+if (n >= 80) {
+   console.log("A");
+  }
+else if (n>=70)
+{ 
+    console.log("B");
+}
+
+else if ( n >=60 ) {
+  console.log("C");
+}
+else {
+  console.log("Pass");
+}
+
