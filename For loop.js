1447b7f60a3ca1d9677b046a06b69b5b8347ad7a
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-04-27 17:41:57 +0000

    Add files via upload
    Javascript For Loop

---
+++ For loop.js
@@ -0,0 +1,22 @@
+//printing text a number of times
+let n=1;
+for (n=1;n<100;n++)
+{
+console.log("Hello World");
+}
+//finding sum of numbers between 1 and 10
+let n=10;
+let sum=0;
+for (i=1;i<=n;i++){
+sum+=i;
+}
+console.log(sum);
+
+//displaying even numbers
+let n=100;
+for (i=2;i<=n;i++)
+{
+console.log(n);
+}
+
+
