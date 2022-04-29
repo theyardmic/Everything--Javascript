843145b1c7890db0b807f24ecdbb0d488ae0ed47
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-04-29 08:29:42 +0000

    Create whileloop.js

---
+++ whileloop.js
@@ -0,0 +1,18 @@
+//While loop for printing text
+let n=1;
+while (n<10)
+{
+console.log("this is a while loop");
+n+=1;
+}
+
+//while loop for sum of numbers between 1 & 10
+let n=1;
+let sum=0;
+while (n<=10)
+{
+ sum=sum+n;
+console.log(sum);
+n+=1;
+}
+
