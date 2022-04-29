de190948b37aa86e10e700c2a0f0190881bc74ad
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-04-29 08:54:56 +0000

    Update dowhileloop.js

--- dowhileloop.js
+++ dowhileloop.js
@@ -1,8 +1,11 @@
 //do while loop
-let n=1;
+let n=1;//initialize the initial number
+//initialize the last number
 const m=10;
 do {
     console.log("this is a do..while loop");
+    //increment
     n++;
-}
+    }
+//setting the while condition
 while (n<=m);
