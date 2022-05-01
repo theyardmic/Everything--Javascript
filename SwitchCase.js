2415521f1931be7061f899b50fa6e5de6165bb34
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-01 06:52:36 +0000

    Create SwitchCase.js
    simple switch case

---
+++ SwitchCase.js
@@ -0,0 +1,17 @@
+// program using switch statement
+let a = 3;
+switch (a) {
+    case 1:
+        a = "one";
+        break;
+    case 2:
+        a = "two";
+        break;
+    case 3:
+        a = "three";
+        break;
+    default:
+        a = "undefined";
+        break;
+        }
+console.log(`The number is ${a}`);
