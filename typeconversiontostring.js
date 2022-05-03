722e2538092a50554b31f05b340aa8d3b011c989
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-03 20:37:07 +0300

    Create typeconversiontostring.js

---
+++ typeconversiontostring.js
@@ -0,0 +1,30 @@
+//number to string
+let result;
+result = String(324);
+console.log(result);  // "324"
+
+result = String(2 + 4);
+console.log(result); // "6"
+
+//other data types to string
+result = String(null);
+console.log(result); // "null"
+
+result = String(undefined);
+console.log(result); // "undefined"
+
+result = String(NaN);
+console.log(result); // "NaN"
+
+result = String(true);
+console.log(result); // "true"
+
+result = String(false);
+console.log(result); // "false"
+
+// using toString()
+result = (324).toString();
+console.log(result); // "324"
+
+result = true.toString();
+console.log(result); // "true"
