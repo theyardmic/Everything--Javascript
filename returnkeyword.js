3ebf6ef644b186d65e41ea911011e5efd6241eba
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-05 21:23:08 +0300

    Update returnkeyword.js
    using the return keyword in javascript

--- returnkeyword.js
+++ returnkeyword.js
@@ -6,7 +6,7 @@ function add(a, b) {
 
 // take input from the user
 let number1 = parseFloat(prompt("Enter first number: "));//taking the first number input
-let number2 = parseFloat(prompt("Enter second number: "));
+let number2 = parseFloat(prompt("Enter second number: "));//taking the second input
 
 // calling function
 let result = add(number1,number2);
