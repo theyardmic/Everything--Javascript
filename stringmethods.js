0dfa6582ba3a6e375e844898e17af25d46168745
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-08 21:05:49 +0300

    Update stringmethods.js

--- stringmethods.js
+++ stringmethods.js
@@ -1,23 +1,23 @@
-const text1 = 'hello';
-const text2 = 'world';
-const text3 = '     JavaScript    ';
+let text1 = 'hello';
+let text2 = 'world';
+let text3 = '     JavaScript    ';
 
 // adding or concatenating two strings
-const result1 = text1.concat(' ', text2);
+let result1 = text1.concat(' ', text2);
 console.log(result1); // "hello world"
 
 // converting the text to uppercase
-const result2 = text1.toUpperCase();
+let result2 = text1.toUpperCase();
 console.log(result2); // HELLO
 
 // removing whitespace from the string
-const result3 = text3.trim();
+let result3 = text3.trim();
 console.log(result3); // JavaScript
 
 //  string to array conversion
-const result4 = text1.split();
+let result4 = text1.split();
 console.log(result4); // ["hello"]
 
 // slicing the string
-const result5= text1.slice(1, 3);
+let result5= text1.slice(1, 3);
 console.log(result5); // "el"
