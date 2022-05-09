301ddfc2a3c1f73c67b129a0bd6e78cd0f25f749
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-09 21:47:12 +0300

    Update area of a triangle.js
    finding area of a triangle with two or more sides

--- area of a triangle.js
+++ area of a triangle.js
@@ -1,14 +1,14 @@
 // JavaScript program to find the area of a triangle with three sides
 
-const side1 = parseInt(prompt('Enter side1: '));
-const side2 = parseInt(prompt('Enter side2: '));
-const side3 = parseInt(prompt('Enter side3: '));
+let side1 = parseInt(prompt('Enter side1: '));
+let side2 = parseInt(prompt('Enter side2: '));
+let side3 = parseInt(prompt('Enter side3: '));
 
 // calculate the semi-perimeter
-const s = (side1 + side2 + side3) / 2;
+let s = (side1 + side2 + side3) / 2;
 
 //calculate the area
-const areaValue = Math.sqrt(
+let areaValue = Math.sqrt(
   s * (s - side1) * (s - side2) * (s - side3)
 );
 
@@ -17,11 +17,11 @@ console.log(
 );
 
 // JavaScript program to find the area of a triangle with base and height
-const baseValue = prompt('Enter the base of a triangle: ');
-const heightValue = prompt('Enter the height of a triangle: ');
+let baseValue = prompt('Enter the base of a triangle: ');
+let heightValue = prompt('Enter the height of a triangle: ');
 
 // calculate the area
-const areaValue = (baseValue * heightValue) / 2;
+let areaValue = (baseValue * heightValue) / 2;
 
 console.log(
   `The area of the triangle is ${areaValue}`
