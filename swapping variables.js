56d8e8118b19ad12dc9c26c9fe568bb642e22f55
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-10 21:21:45 +0300

    Update swapping variables.js

--- swapping variables.js
+++ swapping variables.js
@@ -17,3 +17,16 @@ console.log(`The value of a after swapping: ${a}`);
 console.log(`The value of b after swapping: ${b}`);
  
 //Using opêrators
+//JavaScript program to swap two variables
+
+//take input from the users
+let a = parseInt(prompt('Enter the first variable: '));
+let b = parseInt(prompt('Enter the second variable: '));
+
+// addition and subtraction operator
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
