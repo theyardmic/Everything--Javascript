86aca045b5496ab7fe2815d33a37c0cb59dab4de
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-11 14:34:42 +0300

    Update multiplicationtable.js

--- multiplicationtable.js
+++ multiplicationtable.js
@@ -7,7 +7,7 @@ let number = parseInt(prompt('Enter an integer: '));
 for(let i = 1; i <= 10; i++) {
 
     // multiply i with number
-    const result = i * number;
+  let result = i * number;
 
     // display the result
     console.log(`${number} * ${i} = ${result}`);
