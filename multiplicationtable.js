8aa526caa0420efb5a051d55ec15bbbe6af2298c
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-11 14:37:19 +0300

    Update multiplicationtable.js
    creating multiplication tables using Javascript

--- multiplicationtable.js
+++ multiplicationtable.js
@@ -1,4 +1,4 @@
-// program to generate a multiplication table
+// program to generate a multiplication table up to 10
 
 // take input from the user
 let number = parseInt(prompt('Enter an integer: '));
@@ -10,5 +10,19 @@ for(let i = 1; i <= 10; i++) {
   let result = i * number;
 
     // display the result
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+// program to generate a multiplication table up to a certain range
+
+// take number input from the user
+let number = parseInt(prompt('Enter an integer: '));
+
+// take range input from the user
+let range = parseInt(prompt('Enter a range: '));
+
+//creating a multiplication table
+for(let i = 1; i <= range; i++) {
+  let result = i * number;
     console.log(`${number} * ${i} = ${result}`);
 }
