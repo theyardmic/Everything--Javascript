2c313e2fd70c9c23d687885cc7a332e639ca56f3
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-12 09:46:47 +0300

    Update random number generation.js

--- random number generation.js
+++ random number generation.js
@@ -1,18 +1,25 @@
-//displaying a random number
+//Random Number Generation
+
+//displaying a random number without a range
 let a = Math.random();
 console.log(`The random number is ${a}`);
 
+//displaying random numbers with a range of 1-10
+let a = Math.random()*(10-1)+1;
+console.log(`The Random Number Between 1 & 10 is ${a}`);
+
+//Displaying Integers Between 1 & 10
+let a= Math.floor(Math.random()*(10-1))+1;
+console.log(`The Random Integer Between 1 & 10 is ${a}`);
 
 // displaying integer values between two numbers inclusive of the range from user input
-
 // input from the user
 let min = parseInt(prompt("Enter a min value: "));
 let max = parseInt(prompt("Enter a max value: "));
 
 // generating the random number
 let a = Math.floor(Math.random() * (max - min + 1)) + min;
-
-// display a random number
+// display the random number
 console.log(`Random value between ${min} and ${max} is ${a}`);
 
 
@@ -23,7 +30,3 @@ let max = parseInt(prompt("Enter a max value: "));
 
 // generating the random number
 let a = Math.floor(Math.random() * (max - min)) + min;
-
-// display a random number
-console.log(`Random value between ${min} and ${max} is ${a}`);
-
