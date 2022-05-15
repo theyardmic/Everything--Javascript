93e26e4c88f3d12158388818383e28ba9e9c51cb
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-15 12:01:44 +0300

    Update FInding the maximum and minimum number.js

--- FInding the maximum and minimum number.js
+++ FInding the maximum and minimum number.js
@@ -1,11 +1,11 @@
 // program to find the largest among three numbers
 
 // take input from the user
-const num1 = parseFloat(prompt("Enter first number: "));
-const num2 = parseFloat(prompt("Enter second number: "));
-const num3 = parseFloat(prompt("Enter third number: "));
+let num1 = parseFloat(prompt("Enter first number: "));
+let num2 = parseFloat(prompt("Enter second number: "));
+let num3 = parseFloat(prompt("Enter third number: "));
 
-const largest = Math.max(num1, num2, num3);
+let largest = Math.max(num1, num2, num3);
 
 // display the result
 console.log("The largest number is " + largest);
@@ -13,7 +13,7 @@ console.log("The largest number is " + largest);
 // program to find the largest among three numbers
 
 // take input from the user
-const num1 = parseFloat(prompt("Enter first number: "));
+let num1 = parseFloat(prompt("Enter first number: "));
 const num2 = parseFloat(prompt("Enter second number: "));
 const num3 = parseFloat(prompt("Enter third number: "));
 
