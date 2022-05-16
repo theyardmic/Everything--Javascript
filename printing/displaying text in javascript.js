857994cb6f81c47eb341024bcd458ce667b64b86
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-16 13:04:31 -0400

    Update displaying text in javascript.js
    DISPLAYING TEXT IN JAVASCRIPT

--- printing/displaying text in javascript.js
+++ printing/displaying text in javascript.js
@@ -1,3 +1,8 @@
+//Displaying to the console
 console.log("Hello world");
+
+//Displaying to a pop-up window or dialog box
 alert("Hello World");
+
+//Writing content to HTML
 document.write("Hello World");
