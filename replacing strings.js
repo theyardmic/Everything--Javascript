6ac15a6f7c9d50ce031d9563267da6b5572f54ee
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-17 17:36:30 +0300

    Update replacing strings.js

--- replacing strings.js
+++ replacing strings.js
@@ -1,6 +1,6 @@
 // program to replace all occurrence of a string
 
-const string = 'Mr red has a red house and a red car';
+let string = 'Mr red has a red house and a red car';
 
 const result = string.split('red').join('blue');
 
