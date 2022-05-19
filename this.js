ef8dd2d83b2ff3cd93e5d7f53063bd4cfe94c0af
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-19 22:42:19 +0300

    Update this.js

--- this.js
+++ this.js
@@ -11,3 +11,29 @@ const person = {
 }
 
 person.greet();
+
+const person = {
+    name : 'Jack',
+    age: 25,
+
+    // this inside method
+    // this refers to the object itself
+    greet() {
+        console.log(this);        // {name: "Jack", age ...}
+        console.log(this.age);  // 25
+
+        // inner function
+        function innerFunc() {
+        
+            // this refers to the global object
+            console.log(this);       // Window { ... }
+            console.log(this.age);    // undefined
+            
+        }
+
+        innerFunc();
+
+    }
+}
+
+person.greet();
