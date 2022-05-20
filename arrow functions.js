8435cbd51eb59f25bbacd6cc7e48d23f0b2bd9e7
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-20 16:23:39 +0300

    Update arrow functions.js

--- arrow functions.js
+++ arrow functions.js
@@ -2,3 +2,26 @@
 let greet = () => console.log('Hello');
 greet(); // Hello
 
+
+function Person() {
+    this.name = 'Jack',
+    this.age = 25,
+    this.sayName = function () {
+
+        // this is accessible
+        console.log(this.age);
+
+        function innerFunc() {
+
+            // this refers to the global object
+            console.log(this.age);
+            console.log(this);
+        }
+
+        innerFunc();
+
+    }
+}
+
+let x = new Person();
+x.sayName();
