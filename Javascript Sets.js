848044013025ec10be551f3e9b3e3b616662d41b
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-23 16:45:38 -0400

    Create Javascript Sets.js

---
+++ Javascript Sets.js
@@ -0,0 +1,7 @@
+// create Set
+const set1 = new Set(); // an empty set
+console.log(set1); // Set {}
+
+// Set with multiple types of value
+const set2 = new Set([1, 'hello', {count : true}]);
+console.log(set2); // Set {1, "hello", {count: true}}
