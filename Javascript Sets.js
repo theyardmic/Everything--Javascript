a9ec615123dce79349d1576c68b30d211a63fe84
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-23 16:49:37 -0400

    Update Javascript Sets.js

--- Javascript Sets.js
+++ Javascript Sets.js
@@ -1,14 +1,14 @@
 // create Set
-const set1 = new Set(); // an empty set
+let set1 = new Set(); // an empty set
 console.log(set1); // Set {}
 
 // Set with multiple types of value
-const set2 = new Set([1, 'hello', {count : true}]);
+let set2 = new Set([1, 'hello', {count : true}]);
 console.log(set2); // Set {1, "hello", {count: true}}
 
 //Accessing Set Elements
 
-const set1 = new Set([1, 2, 3]);
+let set1 = new Set([1, 2, 3]);
 
 // access the elements of a Set
 console.log(set1.values()); // Set Iterator [1, 2, 3]
