0c05e9e0e8d4ee9a18c5805e11a88c323af2c71f
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-24 15:39:24 -0400

    Update Javascript Math Methods.js

--- Javascript Math Methods.js
+++ Javascript Math Methods.js
@@ -10,3 +10,9 @@ let power = Math.pow(5, 2);
 console.log(power); 
 
 // Output:  25
+
+//Find the maximum number in a range of values
+let numbers = Math.max(12, 4, 5, 9, 0, -3);
+console.log(numbers); 
+
+// Output:  12
