d26a6c828d71e165266a614704b914654ec77496
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-24 15:37:44 -0400

    Update Javascript Math Methods.js

--- Javascript Math Methods.js
+++ Javascript Math Methods.js
@@ -1,5 +1,12 @@
 //Finding the square root
 // square root of 4
 let number = Math.sqrt(4);
-console.log(number);
+console.log(number); //16
 
+
+//Raising a number to a power
+// 5 raised to power 2
+let power = Math.pow(5, 2);
+console.log(power); 
+
+// Output:  25
