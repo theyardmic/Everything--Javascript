e04be68a26a58e0b380c9eb01259d5e924867554
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-29 14:09:11 -0400

    Update timeout functions.js

--- timeout functions.js
+++ timeout functions.js
@@ -7,3 +7,20 @@ function greet() {
 setTimeout(greet, 3000);
 console.log('This message is shown first');
 
+// program to display time every 3 seconds
+function showTime() {
+
+    // return new date and time
+    let dateTime= new Date();
+
+    // returns the current local time
+    let time = dateTime.toLocaleTimeString();
+
+    console.log(time)
+
+    // display the time after 3 seconds
+     setTimeout(showTime, 3000);
+}
+
+// calling the function
+showTime();
