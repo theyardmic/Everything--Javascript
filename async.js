96eb4f9796e40512821fc72daa5badb4bc724e39
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-30 16:28:47 -0400

    Update async.js

--- async.js
+++ async.js
@@ -16,3 +16,22 @@ async function f() {
 f().then(function(result) {
     console.log(result)
 });
+
+// a promise
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+    resolve('Promise resolved')}, 4000); 
+});
+
+// async function
+async function asyncFunc() {
+
+    // wait until the promise resolves 
+    let result = await promise; 
+
+    console.log(result);
+    console.log('hello');
+}
+
+// calling the async function
+asyncFunc();
