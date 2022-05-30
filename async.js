d6ca85186114e258566f62ac1aa7905d1f19791e
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-30 16:28:19 -0400

    Update async.js

--- async.js
+++ async.js
@@ -6,3 +6,13 @@ async function f() {
 }
 
 f(); 
+
+
+async function f() {
+    console.log('Async function.');
+    return Promise.resolve(1);
+}
+
+f().then(function(result) {
+    console.log(result)
+});
