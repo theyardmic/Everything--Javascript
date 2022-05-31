a6f48da9681a900a9736fb458f49d82d16a7c05a
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-05-31 16:13:40 -0400

    Update set interval.js

--- set interval.js
+++ set interval.js
@@ -5,3 +5,17 @@ function greet() {
 
 setInterval(greet, 1000);
 
+
+// program to display time every 5 seconds
+function showTime() {
+
+    // return new date and time
+    let dateTime= new Date();
+
+    // return the time
+    let time = dateTime.toLocaleTimeString();
+
+    console.log(time)
+}
+
+let display = setInterval(showTime, 5000);
