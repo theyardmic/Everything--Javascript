bf51acb43b14aa881c959fcf2e611dd4c554218b
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-20 11:55:03 +0300

    Update default parameters.js

--- default parameters.js
+++ default parameters.js
@@ -10,12 +10,12 @@ console.log(sum(8, 10));
 //Passing default values as an expression
 function _sumExp(x = 1, y = x, z = x + y) {
  return x + y + z;
- 
-}
+ }
 
 console.log(_sumExp());
 
 
+//Passing a function Value as a default value
 const f = () => 15;
 function fSum(x, h = f() * x){
   return x + h;
