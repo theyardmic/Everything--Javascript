8d6e82732e9b3087233f001d6641396ce1f5a95a
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-20 11:10:02 +0300

    Update default parameters.js

--- default parameters.js
+++ default parameters.js
@@ -5,3 +5,12 @@ function sum(x = 3, y = 5) {
 console.log(8, 10);
 console.log((_sum));
 console.log(_sum(10));
+
+
+function _sumExp(x = 1, y = x, z = x + y) {
+  console.log(y);
+  console.log(z);
+}
+
+
+
