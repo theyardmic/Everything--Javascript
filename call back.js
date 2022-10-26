3ef228c3e8569855f96a1fcaf0c51177605a7ab2
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-26 13:00:31 +0300

    Update call back.js

--- call back.js
+++ call back.js
@@ -11,3 +11,17 @@ function callMe() {
 
 // passing function as an argument
 greet('Brian', callMe);
+
+//  program that shows the delay in execution
+
+function greet() {
+    console.log('Hello world');
+}
+
+function sayName(name) {
+    console.log('Hello' + ' ' + name);
+}
+
+// calling the function
+setTimeout(greet, 2000);
+sayName('John');
