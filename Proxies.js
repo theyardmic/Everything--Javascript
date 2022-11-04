275bd79d62faac5cb16d64886b1e326e42a92332
Strucker.eth <aberamacarleton@gmail.com>
2022-11-05 01:28:15 +0300

    Initializing a Proxy

---
+++ Proxies.js
@@ -0,0 +1,10 @@
+let car = { make: 'Mercedes', model: 'E250' };
+let proxy = new Proxy(car, handler);
+
+const handler = {
+    get: function(car, model) {
+         return car[model];  // return the value of the property
+    }   
+};
+
+console.log(proxy.make); // Mercedes
