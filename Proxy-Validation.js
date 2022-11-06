3dc26029c7ad142d60308534b01c290cafaae5d9
Strucker.eth <aberamacarleton@gmail.com>
2022-11-06 17:39:58 +0300

    Proxy Validation

--- Proxy-Validation.js
+++ Proxy-Validation.js
@@ -1,8 +1,20 @@
-import {car} from './Proxies.js';
+let car = { Make: 'Mercedes', Model: 'E250' };
+
 const handler = {
-    get : function(car, model) {
-        return car[model] ? car[model] : 'Not Found';
+
+    // get the object key and value
+    get(obj, prop) {
+
+    // check condition
+    if (prop == 'Model') {
+      return obj[prop];
+    } else {
+      return 'Not allowed';
     }
+  }
 }
 
-console.log(car.make); //Not Found
+const proxy = new Proxy(car, handler);
+
+console.log(proxy.Model); // E250
+console.log(proxy.Make); // Not allowed
