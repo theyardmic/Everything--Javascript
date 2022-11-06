2f6ae638b47bd974d1c8d303f4d29068dca03957
Strucker.eth <aberamacarleton@gmail.com>
2022-11-06 16:49:23 +0300

    Handler initialization >> Handler access

--- Proxies.js
+++ Proxies.js
@@ -1,12 +1,9 @@
 let car = { make: 'Mercedes', model: 'E250' };
-let proxy = new Proxy(car, handler);
 
 const handler = {
     get: function(car, model) {
          return car[model];  //return the value of the property
     }   
 };
-
-export {car, proxy};
+let proxy = new Proxy(car, handler);
 console.log(proxy.model); //E250
-
