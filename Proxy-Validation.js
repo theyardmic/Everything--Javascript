f5c02c39eaa20b56187c3ac6e32ef32a11fc8d7f
Strucker.eth <aberamacarleton@gmail.com>
2022-11-07 21:12:05 +0300

    Merge edit

--- Proxy-Validation.js
+++ Proxy-Validation.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-let car = { make: 'Mercedes', model: 'E250' };
-
-const handler = {
-    get: function(car, model) {
-         return car[model]  ?  car[model] : "Spec Not Found";  //return the value of the property
-    }   
-};
-
-const proxy = new Proxy(car, handler);
-console.log(proxy.model); //E250
-console.log(proxy.year); //Spec Not Found
-=======
 let car = { Make: 'Mercedes', Model: 'E250' };
 
 const handler = {
@@ -31,4 +18,4 @@ const proxy = new Proxy(car, handler);
 
 console.log(proxy.Model); // E250
 console.log(proxy.Make); // Not allowed
->>>>>>> 03e8e5d1220dc7fce860b17c808a0c4b8d2129d4
+
