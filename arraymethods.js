c5ebb9fa0a68b5b9d157529b8ede9c03b346176e
Strucker.eth <aberamacarleton@gmail.com>
2022-11-14 23:52:47 +0300

    sort method

--- arraymethods.js
+++ arraymethods.js
@@ -14,3 +14,8 @@ cars.forEach(car => console.log(car)); // Mercedes BMW Toyota Genesis
 //find method
 let msrp = [50000, 60000, 70000, 80000];
 let expensiveCars = cars.find(e => e > 60000); // Toyota
+console.log(expensiveCars);
+
+//sort method
+let sortedCars = cars.sort();
+console.log(sortedCars); // BMW Genesis Mercedes Toyota
