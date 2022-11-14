941a2806b1fc7a8b6e9dc15354c59e7ba476dc3c
Strucker.eth <aberamacarleton@gmail.com>
2022-11-14 23:51:43 +0300

    findmethod

--- arraymethods.js
+++ arraymethods.js
@@ -10,3 +10,7 @@ console.log(cars[4]); // Genesis
 
 //foreach method
 cars.forEach(car => console.log(car)); // Mercedes BMW Toyota Genesis
+
+//find method
+let msrp = [50000, 60000, 70000, 80000];
+let expensiveCars = cars.find(e => e > 60000); // Toyota
