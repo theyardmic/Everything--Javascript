2b01c3ccb5d3d431741a657bc010ef3319cb31a2
Strucker.eth <aberamacarleton@gmail.com>
2022-11-14 23:45:34 +0300

    Push method

--- arraymethods.js
+++ arraymethods.js
@@ -2,3 +2,8 @@
 let cars = ["Mercedes", "BMW", "Toyota", "Volvo"];
 let removedCar = cars.pop();
 console.log(removedCar); // Volvo
+
+//push method
+let updatedCars = cars.push("Genesis");
+console.log(updatedCars); // 4
+console.log(cars[4]); // Genesis
