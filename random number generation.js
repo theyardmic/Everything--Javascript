3d62a2cc408622c1338ff5f8500397e21cc2aab0
Strucker.eth <aberamacarleton@gmail.com>
2022-11-15 10:05:58 +0300

    Code Debugging

--- random number generation.js
+++ random number generation.js
@@ -5,22 +5,22 @@ let a = Math.random();
 console.log(`The random number is ${a}`);
 
 //displaying random numbers with a range of 1-10
-let a = Math.random()*(10-1)+1;
-console.log(`The Random Number Between 1 & 10 is ${a}`);
+let b = Math.random()*(10-1)+1;
+console.log(`The Random Number Between 1 & 10 is ${b}`);
 
 //Displaying Integers Between 1 & 10
-let a= Math.floor(Math.random()*(10-1))+1;
-console.log(`The Random Integer Between 1 & 10 is ${a}`);
+let  c = Math.floor(Math.random()*(10-1))+1;
+console.log(`The Random Integer Between 1 & 10 is ${c}`);
 
 // displaying integer values between two numbers inclusive of the range from user input
 // input from the user
-let min = parseInt(prompt("Enter a min value: "));
-let max = parseInt(prompt("Enter a max value: "));
+let minValue = parseInt(prompt("Enter a min value: "));
+let maxValue = parseInt(prompt("Enter a max value: "));
 
 // generating the random number
-let a = Math.floor(Math.random() * (max - min + 1)) + min;
+let randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
 // display the random number
-console.log(`Random value between ${min} and ${max} is ${a}`);
+console.log(`Random value between ${minValue} and ${maxValue} is ${randomNumber}`);
 
 
 //integer values between two numbers exclusive of the range from user input
@@ -29,4 +29,4 @@ let min = parseInt(prompt("Enter a min value: "));
 let max = parseInt(prompt("Enter a max value: "));
 
 // generating the random number
-let a = Math.floor(Math.random() * (max - min)) + min;
+let randomNumber_2 = Math.floor(Math.random() * (max - min)) + min;
